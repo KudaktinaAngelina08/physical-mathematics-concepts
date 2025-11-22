807a0bca5f826b74e690c7219be521d86e65d13b
KudaktinaAngelina08 <kudaktina08@mail.ru>
2025-11-22 08:56:53 +0300

    Создан класс дроби и метод ввода данных.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello, world')
+class Decimal:
+    def __init__(self, numerator=0, denominator=1):
+        if denominator == 0:
+            raise ValueError("Denominator can't be zero")
+        self._numerator = numerator
+        self._denominator = denominator
+
+
+    def input_data(self):
+        try:
+            num = int(input('Введите числитель: '))
+            den = int(input('Введите знаменатель: '))
+
+            if den == 0:
+                raise ValueError("Denominator can't be zero")
+
+            self._numerator = num
+            self._denominator = den
+
+        except ValueError:
+            print('Ошибка ввода данных!')
